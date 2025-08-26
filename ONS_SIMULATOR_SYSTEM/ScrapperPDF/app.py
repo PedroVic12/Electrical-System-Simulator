a20b0dd720454dec3496165c9e9d77751c17b786
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2025-08-26 09:23:56 -0300

    up

--- ONS_SIMULATOR_SYSTEM/ScrapperPDF/app.py
+++ ONS_SIMULATOR_SYSTEM/ScrapperPDF/app.py
@@ -59,8 +59,12 @@ def extract_text_from_must_tables(mode = "folder"):
     print("\n🔚 Script concluído.")
 
 # Caminho da pasta contendo os PDFs
+#! windows
 input_folder = r"C:\Users\pedrovictor.veras\OneDrive - Operador Nacional do Sistema Eletrico\Documentos\ESTAGIO_ONS_PVRV_2025\AUTOMACÕES ONS\arquivos"
-input_folder = r"/home/pedrov12/Documentos/GitHub/Electrical-System-Simulator/ONS_SIMULATOR_SYSTEM/arquivos"
+input_folder = r"C:\Users\pedrovictor.veras\OneDrive - Operador Nacional do Sistema Eletrico\Documentos\ESTAGIO_ONS_PVRV_2025\GitHub\Electrical-System-Simulator\ONS_SIMULATOR_SYSTEM\arquivos"
+
+#!Linux
+#input_folder = r"/home/pedrov12/Documentos/GitHub/Electrical-System-Simulator/ONS_SIMULATOR_SYSTEM/arquivos"
 
 # Configuração dos arquivos PDF e intervalos de páginas
 #! O nome dessa variavel é crucial para o modo "single" nas 2 funções
@@ -70,6 +74,6 @@ intervalos_paginas = ["8-16", "8-24", "7-10", "10-32", "7-13", "7-9"]
 
 if __name__ == "__main__":
 
-    #run_extract_PDF_tables(intervalos_paginas, mode="folder")
+    run_extract_PDF_tables(intervalos_paginas, mode="folder")
 
-    extract_text_from_must_tables(mode ="single")
+    #extract_text_from_must_tables(mode ="single")
