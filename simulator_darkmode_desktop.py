387d37035ae8623ae08aa686d68a8c2f9b512899
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2025-09-07 03:19:53 -0300

    up

--- simulator_darkmode_desktop.py
+++ simulator_darkmode_desktop.py
@@ -18,6 +18,7 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt, Signal, QTimer
 from PySide6.QtGui import QFont, QColor
 
+# styles em arquivo separado pelo CSS
 from styles import AppStyles
 
 
