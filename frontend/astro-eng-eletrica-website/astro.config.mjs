0b2660d30992c85b3cdcd4c26857df39333d7e99
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2025-09-24 13:15:39 -0300

    up

--- frontend/astro-eng-eletrica-website/astro.config.mjs
+++ frontend/astro-eng-eletrica-website/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 // import mdx from "@astrojs/mdx"; // Se for usar MDX para mais funcionalidades
 
+
+import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
-  integrations: [],
+  integrations: [tailwind()],
+
   markdown: {
     remarkPlugins: [],
     rehypePlugins: [],
